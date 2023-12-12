6e90d83929996b6afb5394611595a41e06539bf4
Qaleka <91965947+Qaleka@users.noreply.github.com>
2023-12-12 20:11:07 +0300

    123

---
+++ app/views.py
@@ -0,0 +1,44 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+import time
+import random
+import requests
+
+from concurrent import futures
+
+CALLBACK_URL = "http://localhost:80/api/notifications/"
+
+executor = futures.ThreadPoolExecutor(max_workers=1)
+TOKEN = 'secret_token'
+
+
+def get_random_status(notification_id):
+    time.sleep(5)
+    return {
+        "notification_id": notification_id,
+        "status": bool(random.randint(0, 3)),
+    }
+
+
+def status_callback(task):
+    try:
+        result = task.result()
+        print(result)
+    except futures._base.CancelledError:
+        return
+
+    url = str(CALLBACK_URL+str(result["notification_id"])+'/sending/')
+    requests.put(url, data={"sending_status": result['status'], "token": TOKEN}, timeout=3)
+
+
+@api_view(['POST'])
+def set_status(request):
+    if "notification_id" in request.data.keys():
+        transportation_id = request.data["transportation_id"]
+
+        task = executor.submit(get_random_status, notification_id)
+        task.add_done_callback(status_callback)
+        return Response(status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
